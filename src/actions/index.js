ae8f21275fc8ee197058df09bdee6bcaf5d57135
FlavioVillar <flavio_villar@hotmail.com>
2022-03-31 18:08:52 -0300

    Requisito 01 ok - Crie uma página inicial de login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,10 @@
 // Coloque aqui suas actions
+export const loginAction = (value) => ({
+  type: 'LOGIN',
+  value,
+});
+
+export const deleteCustomer = (value) => ({
+  type: 'DELETE_CUSTOMER',
+  value,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,85 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { loginAction } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      isDisabled: true,
+    };
+    this.validateInputs = this.validateInputs.bind(this);
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      this.setState({ isDisabled: this.validateInputs() });
+    });
+  }
+
+  // alguem@email.com
+  validateInputs() {
+    const { email, password } = this.state;
+    const MIN_PASSWORD_LENGTH = 6;
+    const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    return !regex.test(email) || password.length < MIN_PASSWORD_LENGTH;
+  }
+
   render() {
-    return <div>Login</div>;
+    const { isDisabled, email, password } = this.state;
+    const { login } = this.props;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <div>
+            <input
+              data-testid="email-input"
+              type="text"
+              name="email"
+              value={ email }
+              placeholder="Email"
+              onChange={ this.handleChange }
+            />
+            <input
+              data-testid="password-input"
+              type="password"
+              name="password"
+              value={ password }
+              placeholder="Senha"
+              onChange={ this.handleChange }
+            />
+          </div>
+          <Link to="/carteira">
+            <button
+              type="button"
+              disabled={ isDisabled }
+              onClick={ () => { login(email); } }
+            >
+              Entrar
+
+            </button>
+          </Link>
+        </form>
+
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  login: (email) => dispatch(loginAction(email)),
+});
+
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+export default connect(null, mapDispatchToProps)(Login);
