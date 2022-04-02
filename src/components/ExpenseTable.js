191c6b43392a8ef7ff6903aea2902f322bff27c8
FlavioVillar <flavio_villar@hotmail.com>
2022-04-01 21:21:50 -0300

    Requisito 08 ok - Implemente a lógica para que a tabela seja alimentada pelo estado da aplicação

--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class ExpenseTable extends Component {
   render() {
+    const { expenses } = this.props;
+    // console.log(expenses.map((item) => item.value));
+    // console.log(expenses.map((item) => item.exchangeRates[item.currency].ask));
+    // console.log(expenses.map((item) => item.value * item.exchangeRates[item.currency].ask));
     return (
       <table>
+        <caption>Monthly savings</caption>
         <thead>
           <tr>
             <th>Descrição</th>
@@ -17,19 +23,40 @@ class ExpenseTable extends Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
-        {/* <tbody>
-          <tr>
-            <td>{this.props.value}</td>
-            <td>{this.props.description}</td>
-            <td>{this.props.currency}</td>
-            <td>{this.props.method}</td>
-            <td>{this.props.tag}</td>
-          </tr>
-        </tbody> */}
-      </table>
+        <tbody>
+          {expenses.map((item) => (
+            <tr key={ item.id }>
+              <td>{item.description}</td>
+              <td>{item.tag}</td>
+              <td>{item.method}</td>
+              <td>{Number(item.value).toFixed(2)}</td>
+              <td>
+                {(item.exchangeRates[item.currency].name
+                  .split('/Real Brasileiro'))}
+              </td>
+              <td>{Number(item.exchangeRates[item.currency].ask).toFixed(2)}</td>
+              <td>{Number(item.value) * item.exchangeRates[item.currency].ask}</td>
+              <td>Real</td>
+              <td>
+                <button type="button">Editar</button>
+                <button type="button">Excluir</button>
+              </td>
+            </tr>
+          ))}
 
+        </tbody>
+      </table>
     );
   }
 }
 
-export default ExpenseTable;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+  value: state.wallet.value,
+  description: state.wallet.description,
+  currency: state.wallet.currency,
+  method: state.wallet.method,
+  tag: state.wallet.tag,
+});
+
+export default connect(mapStateToProps)(ExpenseTable);
