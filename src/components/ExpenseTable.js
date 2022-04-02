b63e737208b995aa420244f7070cca7d6ecaca9b
FlavioVillar <flavio_villar@hotmail.com>
2022-04-02 12:58:47 -0300

    Refatorado components ExpenseTable e Header para componente funcional usando HOOKS

--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -1,84 +1,74 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { actionDeleteExpense } from '../actions';
 
-class ExpenseTable extends Component {
-  render() {
-    const { expenses, deleteExpense } = this.props;
-    return (
-      <table>
-        <caption>Monthly savings</caption>
-        <thead>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-        </thead>
-        <tbody>
-          {expenses.map((item) => (
-            <tr key={ item.id }>
-              <td>{item.description}</td>
-              <td>{item.tag}</td>
-              <td>{item.method}</td>
-              <td>{Number(item.value).toFixed(2)}</td>
-              <td>
-                {(item.exchangeRates[item.currency].name
-                  .split('/Real Brasileiro'))}
-              </td>
-              <td>{Number(item.exchangeRates[item.currency].ask).toFixed(2)}</td>
-              <td>{Number(item.value) * item.exchangeRates[item.currency].ask}</td>
-              <td>Real</td>
-              <td>
-                <button type="button" data-testid="edit-btn">Editar</button>
-                <button
-                  type="button"
-                  data-testid="delete-btn"
-                  onClick={ () => deleteExpense(item.id) }
+// refatorado usando hooks
 
-                >
-                  Excluir
+function ExpenseTable() {
+  const { wallet: { expenses } } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  return (
+    <table>
+      <caption>Monthly expenses</caption>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </thead>
 
-                </button>
-              </td>
-            </tr>
-          ))}
-
-        </tbody>
-      </table>
-    );
-  }
+      {
+        !expenses ? ''
+          : (
+            <tbody>
+              {
+                expenses.map((item) => (
+                  <tr key={ item.id }>
+                    <td>{item.description}</td>
+                    <td>{item.tag}</td>
+                    <td>{item.method}</td>
+                    <td>{Number(item.value).toFixed(2)}</td>
+                    <td>
+                      {(item.exchangeRates[item.currency].name
+                        .split('/Real Brasileiro'))}
+                    </td>
+                    <td>{Number(item.exchangeRates[item.currency].ask).toFixed(2)}</td>
+                    <td>
+                      {Number(item.value * item.exchangeRates[item.currency].ask)
+                        .toFixed(2)}
+                    </td>
+                    <td>Real</td>
+                    <td>
+                      <button
+                        type="button"
+                        data-testid="edit-btn"
+                        name="edit"
+                        // onClick={ () => dispatch(actionEditExpense(item.id)) }
+                      >
+                        Editar despesa
+                      </button>
+                      <button
+                        type="button"
+                        data-testid="delete-btn"
+                        onClick={ () => dispatch(actionDeleteExpense(item.id)) }
+                      >
+                        Excluir
+                      </button>
+                    </td>
+                  </tr>))
+              }
+            </tbody>
+          )
+      }
+    </table>
+  );
 }
 
-const mapStateToProps = (state) => ({
-  expenses: state.wallet.expenses,
-  value: state.wallet.value,
-  description: state.wallet.description,
-  currency: state.wallet.currency,
-  method: state.wallet.method,
-  tag: state.wallet.tag,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  deleteExpense: (value) => dispatch({ type: 'DELETE_EXPENSE', value }),
-});
-
-ExpenseTable.propTypes = {
-  expenses: PropTypes.arrayOf(PropTypes.shape({
-    value: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    currency: PropTypes.string.isRequired,
-    method: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-  })).isRequired,
-  deleteExpense: PropTypes.func.isRequired,
-
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ExpenseTable);
+export default ExpenseTable;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,24 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-class Header extends Component {
-    sumTotalValue = () => {
-      const { expenses } = this.props;
-      const sumValue = expenses.map((item) => item)
-        .reduce((acc, { value, exchangeRates, currency }) => acc
+// refatorado para o hooks
+function Header() {
+  const { user: { email }, wallet: { expenses } } = useSelector((state) => state);
+
+  const sumTotalValue = () => {
+    const sumValue = expenses.map((item) => item)
+      .reduce((acc, { value, exchangeRates, currency }) => acc
           + Number(value) * exchangeRates[currency].ask, 0);
-      return sumValue.toFixed(2);
-    }
+    return sumValue.toFixed(2);
+  };
 
-    render() {
-      const { userEmail } = this.props;
-      return (
-        <header>
-          <p data-testid="email-field">{userEmail}</p>
-          <p data-testid="total-field">{this.sumTotalValue()}</p>
-          <p data-testid="header-currency-field">BRL</p>
-        </header>
-      );
-    }
+  return (
+    <header>
+      <p data-testid="email-field">{email}</p>
+      <p data-testid="total-field">{sumTotalValue()}</p>
+      <p data-testid="header-currency-field">BRL</p>
+    </header>
+  );
 }
 
-const mapStateToProps = (state) => ({
-  userEmail: state.user.email,
-  totalExpenses: state.wallet.total,
-  expenses: state.wallet.expenses,
-});
-
-Header.propTypes = {
-  userEmail: PropTypes.string.isRequired,
-  expenses: PropTypes.arrayOf(PropTypes.shape({
-    value: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    currency: PropTypes.string.isRequired,
-  })).isRequired,
-};
-
-export default connect(mapStateToProps)(Header);
+export default Header;
