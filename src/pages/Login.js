d0e364e287386646ee635f98f578e71342dc51a5
FlavioVillar <flavio_villar@hotmail.com>
2022-04-03 11:29:18 -0300

    Login refatorado com Hooks

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,89 +1,78 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { loginAction } from '../actions';
 import wallet from '../img/wallet.png';
 import './Login.css';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      isDisabled: true,
-    };
-  }
+// refatorado usando hooks
 
-  handleChange = ({ target }) => {
-    const { name, value } = target;
-    this.setState({ [name]: value }, () => {
-      this.setState({ isDisabled: this.validateInputs() });
-    });
-  }
-
-  validateInputs = () => {
-    const { email, password } = this.state;
-    const MIN_PASSWORD_LENGTH = 6;
-    const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-    return !regex.test(email) || password.length < MIN_PASSWORD_LENGTH;
-  }
-
-  render() {
-    const { isDisabled, email, password } = this.state;
-    const { login } = this.props;
-    return (
-      <div className="container-login">
-        <div className="Login">
-          <div className="img-wallet">
-            <img className="img-login" src={ wallet } alt="wallet" />
-          </div>
-          <h2 className="text-login">Login</h2>
-          <div className="container-input-login">
-            <input
-              className="input-login"
-              data-testid="email-input"
-              type="text"
-              name="email"
-              value={ email }
-              placeholder="Email"
-              onChange={ this.handleChange }
-            />
-            <input
-              className="input-login"
-              data-testid="password-input"
-              type="password"
-              name="password"
-              value={ password }
-              placeholder="Senha"
-              onChange={ this.handleChange }
-            />
-          </div>
-          <Link to="/carteira">
-            <button
-              className="button-login"
-              type="button"
-              disabled={ isDisabled }
-              onClick={ () => { login(email); } }
-            >
-              Entrar
-            </button>
-          </Link>
-        </div>
-      </div>
-    );
+// validate é criado para ser chamado no useEffect
+function validate(email, password, btnDisabled) {
+  const MIN_PASSWORD_LENGTH = 5;
+  const validateEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/.test(email);
+  const validatePassword = password.length > MIN_PASSWORD_LENGTH;
+  const validInputs = [validateEmail, validatePassword];
+  const isValid = validInputs.every((input) => input);
+  if (isValid) {
+    btnDisabled(false);
+  } else {
+    btnDisabled(true);
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  login: (email) => dispatch(loginAction(email)),
-});
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-Login.propTypes = {
-  login: PropTypes.func.isRequired,
-  history: PropTypes.shape({
-    push: PropTypes.func.isRequired,
-  }).isRequired,
-};
-export default connect(null, mapDispatchToProps)(Login);
+  // Hook useEffect como componentDidMount, componentDidUpdate, e componentWillUnmount combinados.
+  useEffect(() => { validate(email, password, setIsDisabled); }, [email, password]);
+
+  const handleClick = () => {
+    dispatch(loginAction(email));
+    history.push('/carteira');
+  };
+  return (
+    <div className="container-login">
+      <div className="Login">
+        <div className="img-wallet">
+          <img className="img-login" src={ wallet } alt="wallet" />
+        </div>
+        <h2 className="text-login">Login</h2>
+        <div className="container-input-login">
+          <input
+            className="input-login"
+            data-testid="email-input"
+            type="text"
+            name="email"
+            value={ email }
+            placeholder="Email"
+            onChange={ ({ target }) => setEmail(target.value) }
+          />
+          <input
+            className="input-login"
+            data-testid="password-input"
+            type="password"
+            name="password"
+            value={ password }
+            placeholder="Senha"
+            onChange={ ({ target }) => setPassword(target.value) }
+          />
+        </div>
+        <button
+          className="button-login"
+          type="button"
+          disabled={ isDisabled }
+          onClick={ () => handleClick() }
+        >
+          Entrar
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,15 +4,13 @@ import FormToAddExpenses from '../components/FormToAddExpenses';
 import ExpenseTable from '../components/ExpenseTable';
 import './Wallet.css';
 
-class Wallet extends React.Component {
-  render() {
-    return (
-      <div className="container-geral-wallet">
-        <Header />
-        <FormToAddExpenses />
-        <ExpenseTable />
-      </div>);
-  }
+function Wallet() {
+  return (
+    <div className="container-geral-wallet">
+      <Header />
+      <FormToAddExpenses />
+      <ExpenseTable />
+    </div>);
 }
 
 export default Wallet;
